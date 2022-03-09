c62ecad9d52a1ce0d7bf87a5ae439a2795c2dd3f
Paul Deen <paul.deen@dutchworkz.nl>
2022-03-09 10:16:59 +0100

    Removed SkipHasSetterValuesForModelFramework, this gives problems in some scenarios

--- src/ModelFramework.CodeGeneration/CodeGenerationProviders/CsharpExpressionDumperClassBase.cs
+++ src/ModelFramework.CodeGeneration/CodeGenerationProviders/CsharpExpressionDumperClassBase.cs
@@ -40,7 +40,6 @@ public abstract class CSharpExpressionDumperClassBase : ClassBase
             (
                 x => x.AddSingleton<IObjectHandler, BuilderObjectHandler>()
                       .AddSingleton<IObjectHandlerPropertyFilter, SkipDefaultValuesForModelFramework>()
-                      .AddSingleton<IObjectHandlerPropertyFilter, SkipHasSetterValuesForModelFramework>()
                       .AddSingleton<ITypeNameFormatter, CsharpFriendlyNameFormatter>()
                       .AddSingleton<ITypeNameFormatter>(new SkipNamespacesTypeNameFormatter(NamespacesToAbbreviate))
             )
