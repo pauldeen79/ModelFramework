2c64ad07e2964b0e7d794b7e2a7a6a995426b895
Paul Deen <paul.deen@dutchworkz.nl>
2022-06-18 12:38:11 +0200

    Added some unit tests

--- src/ModelFramework.CodeGeneration.Tests/CodeGenerationTests.cs
+++ src/ModelFramework.CodeGeneration.Tests/CodeGenerationTests.cs
@@ -14,7 +14,7 @@ public class CodeGenerationTests
     public void Can_Generate_Model_For_Abstractions()
     {
         // Act & Assert
-        //Verify(GenerateCode.For<TestInterfacesModels>(Settings));
+        ///Verify(GenerateCode.For<TestInterfacesModels>(Settings));
         Verify(GenerateCode.For<ObjectsInterfacesModels>(Settings));
     }
 
@@ -55,7 +55,7 @@ public class CodeGenerationTests
 
         // Act
         GenerateCode.For<TestBuilders>(settings, multipleContentBuilder);
-        GenerateCode.For<TestRecords>(settings, multipleContentBuilder);
+        GenerateCode.For<CodeGenerationProviders.TestRecords>(settings, multipleContentBuilder);
 
         // Assert
         Verify(multipleContentBuilder);
@@ -81,6 +81,124 @@ public class CodeGenerationTests
         actual.NormalizeLineEndings().Should().NotBeNullOrEmpty().And.NotStartWith("Error:");
     }
 
+    [Fact]
+    public void Can_Generate_Plain_Immutable_Class()
+    {
+        // Arrange
+        var settings = new CodeGenerationSettings
+        (
+            basePath: @"C:\Temp\ModelFramework",
+            generateMultipleFiles: false,
+            dryRun: true
+        );
+
+        // Act
+        var generatedCode = GenerateCode.For<TestRecords>(settings);
+        var actual = generatedCode.TemplateFileManager.MultipleContentBuilder.Contents.First().Builder.ToString();
+
+        // Assert
+        actual.NormalizeLineEndings().Should().Be(@"using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+#nullable enable
+    public partial record TestClass
+    {
+        public string TestProperty
+        {
+            get;
+        }
+
+        public TestClass(string testProperty)
+        {
+            this.TestProperty = testProperty;
+            System.ComponentModel.DataAnnotations.Validator.ValidateObject(this, new System.ComponentModel.DataAnnotations.ValidationContext(this, null, null), true);
+        }
+    }
+#nullable restore
+}
+");
+    }
+
+    [Fact]
+    public void Can_Generate_Plain_Immutable_Class_Builder()
+    {
+        // Arrange
+        var settings = new CodeGenerationSettings
+        (
+            basePath: @"C:\Temp\ModelFramework",
+            generateMultipleFiles: false,
+            dryRun: true
+        );
+
+        // Act
+        var generatedCode = GenerateCode.For<TestBuilders>(settings);
+        var actual = generatedCode.TemplateFileManager.MultipleContentBuilder.Contents.First().Builder.ToString();
+
+        // Assert
+        actual.NormalizeLineEndings().Should().Be(@"using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test.Builders
+{
+#nullable enable
+    public partial class estClassBuilder
+    {
+        public string TestProperty
+        {
+            get
+            {
+                return _testPropertyDelegate.Value;
+            }
+            set
+            {
+                _testPropertyDelegate = new (() => value);
+            }
+        }
+
+        public Test.estClass Build()
+        {
+            #pragma warning disable CS8604 // Possible null reference argument.
+            return new Test.estClass(TestProperty);
+            #pragma warning restore CS8604 // Possible null reference argument.
+        }
+
+        public estClassBuilder WithTestProperty(System.Func<string> testPropertyDelegate)
+        {
+            _testPropertyDelegate = new (testPropertyDelegate);
+            return this;
+        }
+
+        public estClassBuilder WithTestProperty(string testProperty)
+        {
+            TestProperty = testProperty;
+            return this;
+        }
+
+        public estClassBuilder()
+        {
+            #pragma warning disable CS8603 // Possible null reference return.
+            _testPropertyDelegate = new (() => string.Empty);
+            #pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        public estClassBuilder(Test.estClass source)
+        {
+            _testPropertyDelegate = new (() => source.TestProperty);
+        }
+
+        private System.Lazy<string> _testPropertyDelegate;
+    }
+#nullable restore
+}
+");
+    }
+
     private void Verify(GenerateCode generatedCode)
     {
         if (Settings.DryRun)
@@ -99,5 +217,35 @@ public class CodeGenerationTests
 
         // Assert
         actual.NormalizeLineEndings().Should().NotBeNullOrEmpty();
+    }
+
+    private abstract class TestBase : CSharpClassBase
+    {
+        public override string Path => @"C:\Temp";
+        public override string DefaultFileName => "GeneratedCode.cs";
+        public override bool RecurseOnDeleteGeneratedFiles => false;
+
+        protected override Type RecordCollectionType => typeof(IReadOnlyCollection<>);
+        protected override bool EnableNullableContext => true;
+        protected override bool CreateCodeGenerationHeader => false;
+
+        protected ITypeBase[] GetModels() => new[]
+        {
+            new ClassBuilder()
+                .WithName("TestClass")
+                .WithNamespace("Test")
+                .AddProperties(new ClassPropertyBuilder().WithName("TestProperty").WithType(typeof(string)))
+                .Build()
+        };
+    }
+
+    private sealed class TestRecords : TestBase
+    {
+        public override object CreateModel() => GetImmutableClasses(GetModels(), "Test");
+    }
+
+    private sealed class TestBuilders : TestBase
+    {
+        public override object CreateModel() => GetImmutableBuilderClasses(GetModels(), "Test", "Test.Builders");
     }
 }
