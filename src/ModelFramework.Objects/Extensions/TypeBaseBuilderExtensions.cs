b31e437d6c7b952b27deb787154d369b3b94fa9f
Paul Deen <paul.deen@dutchworkz.nl>
2022-08-12 16:18:41 +0200

    Minor: Removed newly added method which is unused after refactoring. Not likely to be used anyway - you want to get the full name from the entity, not from the builder

--- src/ModelFramework.Objects/Extensions/TypeBaseBuilderExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ModelFramework.Objects.Extensions;
-
-public static class TypeBaseBuilderExtensions
-{
-    public static string GetFullName(this TypeBaseBuilder instance)
-        => instance.Namespace.GetNamespacePrefix() + instance.Name;
-}
