df73e41cdca2d9255f031cc34bc20acdb94818d5
Paul Deen <paul.deen@dutchworkz.nl>
2022-09-05 12:40:49 +0200

    Fixed detection of nullable generic type arguments in reflection

--- src/ModelFramework.Objects.Tests/Extensions/TypeExtensionsTests.cs
+++ src/ModelFramework.Objects.Tests/Extensions/TypeExtensionsTests.cs
@@ -1,4 +1,6 @@
-﻿namespace ModelFramework.Objects.Tests.Extensions;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ModelFramework.Objects.Tests.Extensions;
 
 public class TypeExtensionsTests
 {
@@ -38,4 +40,80 @@ public class TypeExtensionsTests
         // Assert
         actual.Should().Be("System.Nullable");
     }
+
+    [Fact]
+    public void ToInterface_Maps_Property_Type_With_Nullable_Generic_Argument_Correctly()
+    {
+        // Act
+        var actual = typeof(IMyInterface).ToInterface();
+
+        // Assert
+        actual.Properties.Should().ContainSingle();
+        actual.Properties.Single().TypeName.FixTypeName().Should().Be("System.Func<System.Object?,System.Object?>");
+        actual.Properties.Single().IsNullable.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ToClass_Maps_Field_Type_With_Nullable_Generic_Argument_Correctly()
+    {
+        // Act
+        var actual = typeof(MyClass).ToClass(new ClassSettings());
+
+        // Assert
+        actual.Fields.Should().ContainSingle();
+        actual.Fields.Single().TypeName.FixTypeName().Should().Be("System.Func<System.Object?,System.Object?>");
+        actual.Fields.Single().IsNullable.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ToClass_Maps_Property_Type_Without_Generic_Arguments_Correctly()
+    {
+        // Act
+        var actual = typeof(MyClass).ToClass(new ClassSettings());
+
+        // Assert
+        actual.Properties.Should().ContainSingle();
+        actual.Properties.Single().TypeName.FixTypeName().Should().Be("System.String");
+        actual.Properties.Single().IsNullable.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ToInterface_Maps_Method_Return_Type_With_Nullable_Generic_Argument_Correctly()
+    {
+        // Act
+        var actual = typeof(IMyInterface).ToInterface();
+
+        // Assert
+        actual.Methods.Should().ContainSingle();
+        actual.Methods.Single().TypeName.FixTypeName().Should().Be("System.Func<System.Object?,System.Object?>");
+        actual.Methods.Single().IsNullable.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ToInterface_Maps_Method_Parameter_Type_With_Nullable_Generic_Argument_Correctly()
+    {
+        // Act
+        var actual = typeof(IMyInterface).ToInterface();
+
+        // Assert
+        actual.Methods.Should().ContainSingle();
+        actual.Methods.Single().Parameters.Should().ContainSingle();
+        actual.Methods.Single().Parameters.Single().TypeName.FixTypeName().Should().Be("System.Func<System.Object?,System.Object?>");
+        actual.Methods.Single().Parameters.Single().IsNullable.Should().BeTrue();
+    }
+}
+
+public interface IMyInterface
+{
+    Func<object?, object?>? Property { get; }
+    Func<object?, object?>? Method(Func<object?, object?>? arg);
+}
+
+[ExcludeFromCodeCoverage]
+public class MyClass
+{
+    public string MyProperty { get; } = "";
+#pragma warning disable CA1051 // Do not declare visible instance fields
+    public Func<object?, object?>? Field;
+#pragma warning restore CA1051 // Do not declare visible instance fields
 }
