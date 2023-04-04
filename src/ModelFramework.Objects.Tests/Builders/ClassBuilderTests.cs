37f40c44a76eafe7acac58c4ce24990c98b42ed2
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-04 19:23:53 +0200

    Added missing unit tests

--- src/ModelFramework.Objects.Tests/Builders/ClassBuilderTests.cs
+++ src/ModelFramework.Objects.Tests/Builders/ClassBuilderTests.cs
@@ -25,11 +25,10 @@ public class ClassBuilderTests
         var actual = sut.AddUsings("ModelFramework.Objects.Contracts", "ModelFramework.Database.Contracts");
 
         // Assert
-        actual.Metadata.Select(x => $"{x.Name} = {x.Value}").Should().BeEquivalentTo(new[]
-        {
+        actual.Metadata.Select(x => $"{x.Name} = {x.Value}").Should().BeEquivalentTo(
             $"{MetadataNames.CustomUsing} = ModelFramework.Objects.Contracts",
             $"{MetadataNames.CustomUsing} = ModelFramework.Database.Contracts"
-        });
+        );
     }
 
     [Fact]
@@ -42,10 +41,35 @@ public class ClassBuilderTests
         var actual = sut.AddNamespacesToAbbreviate("ModelFramework.Objects.Contracts", "ModelFramework.Database.Contracts");
 
         // Assert
-        actual.Metadata.Select(x => $"{x.Name} = {x.Value}").Should().BeEquivalentTo(new[]
-        {
+        actual.Metadata.Select(x => $"{x.Name} = {x.Value}").Should().BeEquivalentTo(
             $"{MetadataNames.NamespaceToAbbreviate} = ModelFramework.Objects.Contracts",
             $"{MetadataNames.NamespaceToAbbreviate} = ModelFramework.Database.Contracts"
-        });
+        );
+    }
+
+    [Fact]
+    public void ToString_Gives_Right_Result_With_Namespace_Empty()
+    {
+        // Arrange
+        var sut = new ClassBuilder().WithName("Name");
+
+        // Act
+        var result = sut.ToString();
+
+        // Assert
+        result.Should().Be("Name");
+    }
+
+    [Fact]
+    public void ToString_Gives_Right_Result_With_Namespace_Filled()
+    {
+        // Arrange
+        var sut = new ClassBuilder().WithName("Name").WithNamespace("MyNamespace");
+
+        // Act
+        var result = sut.ToString();
+
+        // Assert
+        result.Should().Be("MyNamespace.Name");
     }
 }
---
+++ src/ModelFramework.Objects.Tests/Builders/ClassFieldBuilderTests.cs
@@ -0,0 +1,30 @@
+﻿namespace ModelFramework.Objects.Tests.Builders;
+
+public class ClassFieldBuilderTests
+{
+    [Fact]
+    public void ToString_Gives_Right_Result_With_ParentTypeFullName_Filled()
+    {
+        // Arrange
+        var sut = new ClassFieldBuilder().WithName("Name").WithType(typeof(int));
+
+        // Act
+        var result = sut.ToString();
+
+        // Assert
+        result.Should().Be("System.Int32 Name");
+    }
+
+    [Fact]
+    public void ToString_Gives_Right_Result_With_ParentTypeFullName_Empty()
+    {
+        // Arrange
+        var sut = new ClassFieldBuilder().WithName("Name").WithType(typeof(int)).WithParentTypeFullName("MyParent");
+
+        // Act
+        var result = sut.ToString();
+
+        // Assert
+        result.Should().Be("System.Int32 MyParent.Name");
+    }
+}
