5b70b5421deb0eb35cf3c30fe4c6a41f3f9edfcf
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-18 20:42:59 +0200

    Added missing unit tests

---
+++ src/ModelFramework.Objects.Tests/Builders/ParameterBuilderExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿namespace ModelFramework.Objects.Tests.Builders;
+
+public class ParameterBuilderExtensionsTests
+{
+    [Fact]
+    public void ToString_Gives_Right_Result_With_Ref_Parameter()
+    {
+        // Arrange
+        var parameter = new ParameterBuilder().WithName("Test").WithType(typeof(int)).WithIsRef();
+
+        // Act
+        var result = parameter.ToString();
+
+        // Assert
+        result.Should().Be("ref System.Int32 Test");
+    }
+
+    [Fact]
+    public void ToString_Gives_Right_Result_With_Out_Parameter()
+    {
+        // Arrange
+        var parameter = new ParameterBuilder().WithName("Test").WithType(typeof(int)).WithIsOut();
+
+        // Act
+        var result = parameter.ToString();
+
+        // Assert
+        result.Should().Be("out System.Int32 Test");
+    }
+}
