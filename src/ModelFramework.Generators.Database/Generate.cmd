a868687c4fce5dccf25f43954ab0235cdb84eb58
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-24 12:15:40 +0200

    Regenerated generators with latest version of t4plus

--- src/ModelFramework.Generators.Database/Generate.cmd
+++ src/ModelFramework.Generators.Database/Generate.cmd
@@ -1,3 +1,3 @@
-C:
-t4plus source -f "C:\Git\ModelFramework\src\ModelFramework.Generators.Database\Templates\SqlServerDatabaseSchemaGenerator.template" -o "C:\Git\ModelFramework\src\ModelFramework.Generators.Database\SqlServerDatabaseSchemaGenerator.generated.cs" -Parameters $T4Plus.BasePath:C:\Git\ModelFramework\src\
+D:
+t4plus source -f "D:\Git\ModelFramework\src\ModelFramework.Generators.Database\Templates\SqlServerDatabaseSchemaGenerator.template" -o "D:\Git\ModelFramework\src\ModelFramework.Generators.Database\SqlServerDatabaseSchemaGenerator.generated.cs" -Parameters $T4Plus.BasePath:D:\Git\ModelFramework\src\
 pause
--- src/ModelFramework.Generators.Objects/Generate.cmd
+++ src/ModelFramework.Generators.Objects/Generate.cmd
@@ -1,3 +1,3 @@
-C:
-t4plus source -f "C:\Git\ModelFramework\src\ModelFramework.Generators.Objects\Templates\CSharpClassGenerator.template" -o "C:\Git\ModelFramework\src\ModelFramework.Generators.Objects\CSharpClassGenerator.generated.cs" -Parameters $T4Plus.BasePath:C:\Git\ModelFramework\src\
+D:
+t4plus source -f "D:\Git\ModelFramework\src\ModelFramework.Generators.Objects\Templates\CSharpClassGenerator.template" -o "D:\Git\ModelFramework\src\ModelFramework.Generators.Objects\CSharpClassGenerator.generated.cs" -Parameters $T4Plus.BasePath:D:\Git\ModelFramework\src\
 pause
