411956a41702ab45e95fe9672849269ebe6042dc
Paul Deen <paul.deen@dutchworkz.nl>
2023-09-28 13:29:33 +0200

    Simplified AttributeBuilder a little bit

--- src/ModelFramework.Objects/Builders/AttributeBuilder.cs
+++ src/ModelFramework.Objects/Builders/AttributeBuilder.cs
@@ -6,34 +6,34 @@ public partial class AttributeBuilder
         => sourceAttribute switch
         {
             StringLengthAttribute stringLengthAttribute =>
-                new AttributeBuilder(stringLengthAttribute.GetType())
+                new AttributeBuilder(stringLengthAttribute)
                     .AddParameters(new AttributeParameterBuilder().WithValue(stringLengthAttribute.MaximumLength))
                     .AddParameters(CreateConditional(() => stringLengthAttribute.MinimumLength > 0, new AttributeParameterBuilder().WithValue(stringLengthAttribute.MinimumLength).WithName(nameof(stringLengthAttribute.MinimumLength))))
                     .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(stringLengthAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(stringLengthAttribute.ErrorMessage).WithName(nameof(stringLengthAttribute.ErrorMessage)))),
             RangeAttribute rangeAttribute =>
-                new AttributeBuilder(rangeAttribute.GetType())
+                new AttributeBuilder(rangeAttribute)
                     .AddParameters(new AttributeParameterBuilder().WithValue(rangeAttribute.Minimum))
                     .AddParameters(new AttributeParameterBuilder().WithValue(rangeAttribute.Maximum))
                     .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(rangeAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(rangeAttribute.ErrorMessage).WithName(nameof(RangeAttribute.ErrorMessage)))),
             MinLengthAttribute minLengthAttribute =>
-                new AttributeBuilder(minLengthAttribute.GetType())
+                new AttributeBuilder(minLengthAttribute)
                     .AddParameters(new AttributeParameterBuilder().WithValue(minLengthAttribute.Length))
                     .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(minLengthAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(minLengthAttribute.ErrorMessage).WithName(nameof(MinLengthAttribute.ErrorMessage)))),
             MaxLengthAttribute maxLengthAttribute =>
-                new AttributeBuilder(maxLengthAttribute.GetType())
+                new AttributeBuilder(maxLengthAttribute)
                     .AddParameters(new AttributeParameterBuilder().WithValue(maxLengthAttribute.Length))
                     .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(maxLengthAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(maxLengthAttribute.ErrorMessage).WithName(nameof(MaxLengthAttribute.ErrorMessage)))),
             RegularExpressionAttribute regularExpressionAttribute =>
-                new AttributeBuilder(regularExpressionAttribute.GetType())
+                new AttributeBuilder(regularExpressionAttribute)
                     .AddParameters(new AttributeParameterBuilder().WithValue(regularExpressionAttribute.Pattern))
                     .AddParameters(CreateConditional(() => regularExpressionAttribute.MatchTimeoutInMilliseconds != 2000, new AttributeParameterBuilder().WithValue(regularExpressionAttribute.MatchTimeoutInMilliseconds).WithName(nameof(RegularExpressionAttribute.MatchTimeoutInMilliseconds))))
                     .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(regularExpressionAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(regularExpressionAttribute.ErrorMessage).WithName(nameof(RegularExpressionAttribute.ErrorMessage)))),
             RequiredAttribute requiredAttribute =>
-                new AttributeBuilder(requiredAttribute.GetType())
+                new AttributeBuilder(requiredAttribute)
                     .AddParameters(CreateConditional(() => requiredAttribute.AllowEmptyStrings, new AttributeParameterBuilder().WithValue(requiredAttribute.AllowEmptyStrings).WithName(nameof(RequiredAttribute.AllowEmptyStrings))))
                     .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(requiredAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(requiredAttribute.ErrorMessage).WithName(nameof(RequiredAttribute.ErrorMessage)))),
             UrlAttribute urlAttribute =>
-                new AttributeBuilder(urlAttribute.GetType())
+                new AttributeBuilder(urlAttribute)
                     .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(urlAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(urlAttribute.ErrorMessage).WithName(nameof(UrlAttribute.ErrorMessage)))),
             _ => new AttributeBuilder().WithName(sourceAttribute.GetType().FullName)
         };
@@ -44,11 +44,6 @@ public partial class AttributeBuilder
         {
             yield return result;
         }
-    }
-
-    public AttributeBuilder(Type type) : this()
-    {
-        Name = type.FullName;
     }
 
     public AttributeBuilder(System.Attribute source, Func<System.Attribute, AttributeBuilder?>? initializeDelegate = null) : this()
