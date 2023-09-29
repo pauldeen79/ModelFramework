58ad185a26658d1b57aae11b2da2f2b70d0e0b4e
Paul Deen <paul.deen@dutchworkz.nl>
2023-09-29 12:45:38 +0200

    Minor: Reorganized tests for builder pipeline builder

--- src/ClassFramework.Pipelines.Tests/BuilderPipelineBuilderTests.cs
+++ src/ClassFramework.Pipelines.Tests/BuilderPipelineBuilderTests.cs
@@ -2,50 +2,56 @@
 
 public class BuilderPipelineBuilderTests
 {
-    [Fact]
-    public void Can_Build_Pipeline()
+    public class Build
     {
-        // Arrange
-        var builder = new BuilderPipelineBuilder();
+        [Fact]
+        public void Builds_Pipeline()
+        {
+            // Arrange
+            var builder = new BuilderPipelineBuilder();
 
-        // Act
-        var result = builder.Build();
+            // Act
+            var result = builder.Build();
 
-        // Assert
-        result.Should().NotBeNull();
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Builds_Altered_Pipeline_Using_Existing_Pipeline()
+        {
+            // Arrange
+            var sourcePipeline = new BuilderPipelineBuilder().Build();
+
+            // Act
+            var pipeline = new BuilderPipelineBuilder(sourcePipeline)
+                .With(x => x.Features.Clear())
+                .Build();
+
+            // Assert
+            pipeline.Features.Should().BeEmpty();
+        }
     }
 
-    [Fact]
-    public void Can_Alter_Pipeline_Using_Builder()
+    public class Process
     {
-        // Arrange
-        var sourcePipeline = new BuilderPipelineBuilder().Build();
+        [Fact]
+        public void Makes_All_Properties_Writable()
+        {
+            // Arrange
+            var model = new ClassBuilder()
+                .AddProperties(
+                    new ClassPropertyBuilder().WithName("Property1").WithHasSetter(false),
+                    new ClassPropertyBuilder().WithName("Property2").WithHasSetter(true)
+                );
+            var context = new BuilderPipelineBuilderSettings();
+            var pipeline = new BuilderPipelineBuilder().Build();
 
-        // Act
-        var pipeline = new BuilderPipelineBuilder(sourcePipeline)
-            .With(x => x.Features.Clear())
-            .Build();
+            // Act
+            pipeline.Process(model, context);
 
-        // Assert
-        pipeline.Features.Should().BeEmpty();
-    }
-
-    [Fact]
-    public void Process_Makes_All_Properties_Writable()
-    {
-        // Arrange
-        var model = new ClassBuilder()
-            .AddProperties(
-                new ClassPropertyBuilder().WithName("Property1").WithHasSetter(false),
-                new ClassPropertyBuilder().WithName("Property2").WithHasSetter(true)
-            );
-        var context = new BuilderPipelineBuilderSettings();
-        var pipeline = new BuilderPipelineBuilder().Build();
-
-        // Act
-        pipeline.Process(model, context);
-
-        // Assert
-        model.Properties.Select(x => x.HasSetter).Should().AllBeEquivalentTo(true);
+            // Assert
+            model.Properties.Select(x => x.HasSetter).Should().AllBeEquivalentTo(true);
+        }
     }
 }
