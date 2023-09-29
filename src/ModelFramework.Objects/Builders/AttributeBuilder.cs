d0438b6d1b2317c98e300153b2c38e80ab5f8bd6
Paul Deen <paul.deen@dutchworkz.nl>
2023-09-29 08:14:19 +0200

    Refactored switch expression to remove code duplication

--- src/ModelFramework.Objects/Builders/AttributeBuilder.cs
+++ src/ModelFramework.Objects/Builders/AttributeBuilder.cs
@@ -9,40 +9,37 @@ public partial class AttributeBuilder
                 new AttributeBuilder(stringLengthAttribute)
                     .AddParameters(new AttributeParameterBuilder().WithValue(stringLengthAttribute.MaximumLength))
                     .AddParameters(CreateConditional(() => stringLengthAttribute.MinimumLength > 0, new AttributeParameterBuilder().WithValue(stringLengthAttribute.MinimumLength).WithName(nameof(stringLengthAttribute.MinimumLength))))
-                    .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(stringLengthAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(stringLengthAttribute.ErrorMessage).WithName(nameof(stringLengthAttribute.ErrorMessage)))),
+                    .AddParameters(ErrorMessage(stringLengthAttribute)),
             RangeAttribute rangeAttribute =>
                 new AttributeBuilder(rangeAttribute)
                     .AddParameters(new AttributeParameterBuilder().WithValue(rangeAttribute.Minimum))
                     .AddParameters(new AttributeParameterBuilder().WithValue(rangeAttribute.Maximum))
-                    .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(rangeAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(rangeAttribute.ErrorMessage).WithName(nameof(RangeAttribute.ErrorMessage)))),
+                    .AddParameters(ErrorMessage(rangeAttribute)),
             MinLengthAttribute minLengthAttribute =>
                 new AttributeBuilder(minLengthAttribute)
                     .AddParameters(new AttributeParameterBuilder().WithValue(minLengthAttribute.Length))
-                    .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(minLengthAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(minLengthAttribute.ErrorMessage).WithName(nameof(MinLengthAttribute.ErrorMessage)))),
+                    .AddParameters(ErrorMessage(minLengthAttribute)),
             MaxLengthAttribute maxLengthAttribute =>
                 new AttributeBuilder(maxLengthAttribute)
                     .AddParameters(new AttributeParameterBuilder().WithValue(maxLengthAttribute.Length))
-                    .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(maxLengthAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(maxLengthAttribute.ErrorMessage).WithName(nameof(MaxLengthAttribute.ErrorMessage)))),
+                    .AddParameters(ErrorMessage(maxLengthAttribute)),
             RegularExpressionAttribute regularExpressionAttribute =>
                 new AttributeBuilder(regularExpressionAttribute)
                     .AddParameters(new AttributeParameterBuilder().WithValue(regularExpressionAttribute.Pattern))
                     .AddParameters(CreateConditional(() => regularExpressionAttribute.MatchTimeoutInMilliseconds != 2000, new AttributeParameterBuilder().WithValue(regularExpressionAttribute.MatchTimeoutInMilliseconds).WithName(nameof(RegularExpressionAttribute.MatchTimeoutInMilliseconds))))
-                    .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(regularExpressionAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(regularExpressionAttribute.ErrorMessage).WithName(nameof(RegularExpressionAttribute.ErrorMessage)))),
+                    .AddParameters(ErrorMessage(regularExpressionAttribute)),
             RequiredAttribute requiredAttribute =>
                 new AttributeBuilder(requiredAttribute)
                     .AddParameters(CreateConditional(() => requiredAttribute.AllowEmptyStrings, new AttributeParameterBuilder().WithValue(requiredAttribute.AllowEmptyStrings).WithName(nameof(RequiredAttribute.AllowEmptyStrings))))
-                    .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(requiredAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(requiredAttribute.ErrorMessage).WithName(nameof(RequiredAttribute.ErrorMessage)))),
-            UrlAttribute urlAttribute =>
-                new AttributeBuilder(urlAttribute)
-                    .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(urlAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(urlAttribute.ErrorMessage).WithName(nameof(UrlAttribute.ErrorMessage)))),
-            PhoneAttribute phoneAttribute =>
-                new AttributeBuilder(phoneAttribute)
-                    .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(phoneAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(phoneAttribute.ErrorMessage).WithName(nameof(UrlAttribute.ErrorMessage)))),
-            EmailAddressAttribute emailAddressAttribute =>
-                new AttributeBuilder(emailAddressAttribute)
-                    .AddParameters(CreateConditional(() => !string.IsNullOrEmpty(emailAddressAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(emailAddressAttribute.ErrorMessage).WithName(nameof(UrlAttribute.ErrorMessage)))),
+                    .AddParameters(ErrorMessage(requiredAttribute)),
+            ValidationAttribute validationAttribute =>
+                new AttributeBuilder(validationAttribute)
+                    .AddParameters(ErrorMessage(validationAttribute)),
             _ => new AttributeBuilder().WithName(sourceAttribute.GetType().FullName)
         };
+
+    private static IEnumerable<AttributeParameterBuilder> ErrorMessage(ValidationAttribute validationAttribute)
+        => CreateConditional(() => !string.IsNullOrEmpty(validationAttribute.ErrorMessage), new AttributeParameterBuilder().WithValue(validationAttribute.ErrorMessage).WithName(nameof(ValidationAttribute.ErrorMessage)));
 
     private static IEnumerable<AttributeParameterBuilder> CreateConditional(Func<bool> condition, AttributeParameterBuilder result)
     {
