3c3c44ca2938dbffb5b62ae2e14100ba610014f6
Paul Deen <paul.deen@dutchworkz.nl>
2023-09-30 08:35:15 +0200

    Filled empty records

--- src/ClassFramework.Pipelines/BuilderPipelineBuilderConstructorSettings.cs
+++ src/ClassFramework.Pipelines/BuilderPipelineBuilderConstructorSettings.cs
@@ -2,4 +2,20 @@
 
 public record BuilderPipelineBuilderConstructorSettings
 {
+    public bool AddCopyConstructor { get; }
+    public bool AddConstructorWithAllProperties { get; }
+    public bool AddNullChecks { get; }
+    public bool SetDefaultValues { get; }
+
+    public BuilderPipelineBuilderConstructorSettings(
+        bool addCopyConstructor = false,
+        bool addConstructorWithAllProperties = false,
+        bool addNullChecks = false,
+        bool setDefaultValues = true)
+    {
+        AddCopyConstructor = addCopyConstructor;
+        AddConstructorWithAllProperties = addConstructorWithAllProperties;
+        AddNullChecks = addNullChecks;
+        SetDefaultValues = setDefaultValues;
+    }
 }
--- src/ClassFramework.Pipelines/BuilderPipelineBuilderGenerationSettings.cs
+++ src/ClassFramework.Pipelines/BuilderPipelineBuilderGenerationSettings.cs
@@ -2,4 +2,20 @@
 
 public record BuilderPipelineBuilderGenerationSettings
 {
+    public bool UseLazyInitialization { get; }
+    public bool CopyPropertyCode { get; }
+    public bool CopyFields { get; }
+    public bool CopyAttributes { get; }
+
+    public BuilderPipelineBuilderGenerationSettings(
+        bool useLazyInitialization = false,
+        bool copyPropertyCode = true,
+        bool copyFields = true,
+        bool copyAttributes = true)
+    {
+        UseLazyInitialization = useLazyInitialization;
+        CopyPropertyCode = copyPropertyCode;
+        CopyFields = copyFields;
+        CopyAttributes = copyAttributes;
+    }
 }
