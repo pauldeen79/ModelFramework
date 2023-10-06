36f67085f768e686d32f12ceef3b95103e9fbe8d
Paul Deen <paul.deen@dutchworkz.nl>
2023-10-06 10:06:55 +0200

    Ported unit tests for GetFullName from builders to domain

--- src/ClassFramework.Domain.Tests/GlobalUsings.cs
+++ src/ClassFramework.Domain.Tests/GlobalUsings.cs
@@ -1,3 +1,4 @@
 ﻿global using ClassFramework.Domain.Extensions;
+global using ClassFramework.Domain.Types;
 global using FluentAssertions;
 global using Xunit;
---
+++ src/ClassFramework.Domain.Tests/TypeBaseTests.cs
@@ -0,0 +1,33 @@
+﻿namespace ClassFramework.Domain.Tests;
+
+public class TypeBaseTests
+{
+    public class GetFullName
+    {
+        [Fact]
+        public void Returns_Full_Name_When_Namespace_Is_Present()
+        {
+            // Arrange
+            var sut = new Class(Enumerable.Empty<ClassField>(), default, default, default, Enumerable.Empty<Class>(), Enumerable.Empty<ClassConstructor>(), Enumerable.Empty<Enum>(), default, default, "MyNamespace", default, Enumerable.Empty<string>(), Enumerable.Empty<ClassProperty>(), Enumerable.Empty<ClassMethod>(), Enumerable.Empty<string>(), Enumerable.Empty<string>(), Enumerable.Empty<string>(), Enumerable.Empty<Metadata>(), default, "MyClass", Enumerable.Empty<Attribute>());
+
+            // Act
+            var result = sut.GetFullName();
+
+            // Assert
+            result.Should().Be("MyNamespace.MyClass");
+        }
+
+        [Fact]
+        public void Returns_Name_When_Namespace_Is_Not_Present()
+        {
+            // Arrange
+            var sut = new Class(Enumerable.Empty<ClassField>(), default, default, default, Enumerable.Empty<Class>(), Enumerable.Empty<ClassConstructor>(), Enumerable.Empty<Enum>(), default, default, string.Empty, default, Enumerable.Empty<string>(), Enumerable.Empty<ClassProperty>(), Enumerable.Empty<ClassMethod>(), Enumerable.Empty<string>(), Enumerable.Empty<string>(), Enumerable.Empty<string>(), Enumerable.Empty<Metadata>(), default, "MyClass", Enumerable.Empty<Attribute>());
+
+            // Act
+            var result = sut.GetFullName();
+
+            // Assert
+            result.Should().Be("MyClass");
+        }
+    }
+}
