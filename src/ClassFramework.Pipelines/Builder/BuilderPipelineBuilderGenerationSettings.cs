19d756ccd3cde9e925534d9167c98a9d6882317f
Paul Deen <paul.deen@dutchworkz.nl>
2023-10-07 15:27:25 +0200

    Move EnableNullableReferenceTypes to GenerationSettings as well...

--- src/ClassFramework.Pipelines/Builder/BuilderPipelineBuilderGenerationSettings.cs
+++ src/ClassFramework.Pipelines/Builder/BuilderPipelineBuilderGenerationSettings.cs
@@ -3,15 +3,18 @@
 public record BuilderPipelineBuilderGenerationSettings
 {
     public bool AddNullChecks { get; }
+    public bool EnableNullableReferenceTypes { get; }
     public bool CopyFields { get; }
     public bool CopyAttributes { get; }
 
     public BuilderPipelineBuilderGenerationSettings(
         bool addNullChecks = false,
+        bool enableNullableReferenceTypes = false,
         bool copyFields = true,
         bool copyAttributes = true)
     {
         AddNullChecks = addNullChecks;
+        EnableNullableReferenceTypes = enableNullableReferenceTypes;
         CopyFields = copyFields;
         CopyAttributes = copyAttributes;
     }
--- src/ClassFramework.Pipelines/Builder/BuilderPipelineBuilderTypeSettings.cs
+++ src/ClassFramework.Pipelines/Builder/BuilderPipelineBuilderTypeSettings.cs
@@ -3,16 +3,13 @@
 public record BuilderPipelineBuilderTypeSettings
 {
     public string NewCollectionTypeName { get; }
-    public bool EnableNullableReferenceTypes { get; }
     public Func<TypeBase, bool, string>? FormatInstanceTypeNameDelegate { get; }
 
     public BuilderPipelineBuilderTypeSettings(
         string newCollectionTypeName = "System.Collections.Generic.List",
-        bool enableNullableReferenceTypes = false,
         Func<TypeBase, bool, string>? formatInstanceTypeNameDelegate = null)
     {
         NewCollectionTypeName = newCollectionTypeName;
-        EnableNullableReferenceTypes = enableNullableReferenceTypes;
         FormatInstanceTypeNameDelegate = formatInstanceTypeNameDelegate;
     }
 }
