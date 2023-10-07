3f3b919194d0b2dedb4591f87e7811b255e0414a
Paul Deen <paul.deen@dutchworkz.nl>
2023-10-07 15:25:39 +0200

    Refactor, important to do this before porting/recreating the code: Move property to correct settings class

--- src/ClassFramework.Pipelines/Builder/BuilderPipelineBuilderConstructorSettings.cs
+++ src/ClassFramework.Pipelines/Builder/BuilderPipelineBuilderConstructorSettings.cs
@@ -3,16 +3,13 @@
 public record BuilderPipelineBuilderConstructorSettings
 {
     public bool AddCopyConstructor { get; }
-    public bool AddNullChecks { get; }
     public bool SetDefaultValues { get; }
 
     public BuilderPipelineBuilderConstructorSettings(
         bool addCopyConstructor = false,
-        bool addNullChecks = false,
         bool setDefaultValues = true)
     {
         AddCopyConstructor = addCopyConstructor;
-        AddNullChecks = addNullChecks;
         SetDefaultValues = setDefaultValues;
     }
 }
--- src/ClassFramework.Pipelines/Builder/BuilderPipelineBuilderGenerationSettings.cs
+++ src/ClassFramework.Pipelines/Builder/BuilderPipelineBuilderGenerationSettings.cs
@@ -2,13 +2,16 @@
 
 public record BuilderPipelineBuilderGenerationSettings
 {
+    public bool AddNullChecks { get; }
     public bool CopyFields { get; }
     public bool CopyAttributes { get; }
 
     public BuilderPipelineBuilderGenerationSettings(
+        bool addNullChecks = false,
         bool copyFields = true,
         bool copyAttributes = true)
     {
+        AddNullChecks = addNullChecks;
         CopyFields = copyFields;
         CopyAttributes = copyAttributes;
     }
