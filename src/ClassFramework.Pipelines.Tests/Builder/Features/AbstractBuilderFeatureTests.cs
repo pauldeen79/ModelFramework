016e3d5fe9e21698622fa44cae88d15e1b305910
Paul Deen <paul.deen@dutchworkz.nl>
2023-10-07 14:20:17 +0200

    Added some missing unit tests

---
+++ src/ClassFramework.Pipelines.Tests/Builder/Features/AbstractBuilderFeatureTests.cs
@@ -0,0 +1,81 @@
+﻿namespace ClassFramework.Pipelines.Tests.Builder.Features;
+
+public class AbstractBuilderFeatureTests : TestBase<AbstractBuilderFeature>
+{
+    public class Process : AbstractBuilderFeatureTests
+    {
+        [Fact]
+        public void Throws_On_Null_Context()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act & Assert
+            sut.Invoking(x => x.Process(context: null!))
+               .Should().Throw<ArgumentNullException>().WithParameterName("context");
+        }
+
+        [Fact]
+        public void Adds_AddGenericTypeArguments_When_IsBuilderForAbstractEntity_Is_True()
+        {
+            // Arrange
+            var sourceModel = new ClassBuilder().WithName("SomeClass").WithNamespace("SomeNamespace").Build();
+            var parser = Fixture.Freeze<IFormattableStringParser>();
+            parser.Parse(Arg.Any<string>(), Arg.Any<IFormatProvider>(), Arg.Any<object?>())
+                  .Returns(x => Result<string>.Success(x.ArgAt<string>(0).Replace("{Name}", sourceModel.Name, StringComparison.Ordinal)));
+            var sut = CreateSut();
+            var model = new ClassBuilder();
+            var settings = new BuilderPipelineBuilderSettings(inheritanceSettings: new BuilderPipelineBuilderInheritanceSettings(enableEntityInheritance: true));
+            var context = new PipelineContext<ClassBuilder, BuilderPipelineBuilderContext>(model, new BuilderPipelineBuilderContext(sourceModel, settings, CultureInfo.InvariantCulture));
+
+            // Act
+            sut.Process(context);
+
+            // Assert
+            model.Interfaces.Should().BeEmpty();
+            model.GenericTypeArguments.Should().BeEquivalentTo("TBuilder", "TEntity");
+            model.GenericTypeArgumentConstraints.Should().BeEquivalentTo("where TEntity : SomeNamespace.SomeClass", "where TBuilder : SomeClassBuilder<TBuilder, TEntity>");
+            model.Abstract.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Adds_IValidatableObject_Interface_When_IsBuilderForAbstractEntity_Is_False_And_Validation_Is_Shared_Between_Builder_And_Entity()
+        {
+            // Arrange
+            var sourceModel = new ClassBuilder().WithName("SomeClass").WithNamespace("SomeNamespace").Build();
+            var sut = CreateSut();
+            var model = new ClassBuilder();
+            var settings = new BuilderPipelineBuilderSettings(classSettings: new ImmutableClassPipelineBuilderSettings(constructorSettings: new ImmutableClassPipelineBuilderConstructorSettings(validateArguments: ArgumentValidationType.Shared)));
+            var context = new PipelineContext<ClassBuilder, BuilderPipelineBuilderContext>(model, new BuilderPipelineBuilderContext(sourceModel, settings, CultureInfo.InvariantCulture));
+
+            // Act
+            sut.Process(context);
+
+            // Assert
+            model.Interfaces.Should().BeEquivalentTo("System.ComponentModel.DataAnnotations.IValidatableObject");
+            model.GenericTypeArguments.Should().BeEmpty();
+            model.GenericTypeArgumentConstraints.Should().BeEmpty();
+            model.Abstract.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Does_Not_Add_IValidatableObject_Interface_When_IsBuilderForAbstractEntity_Is_False_And_Validation_Is_Not_Shared_Between_Builder_And_Entity()
+        {
+            // Arrange
+            var sourceModel = new ClassBuilder().WithName("SomeClass").WithNamespace("SomeNamespace").Build();
+            var sut = CreateSut();
+            var model = new ClassBuilder();
+            var settings = new BuilderPipelineBuilderSettings();
+            var context = new PipelineContext<ClassBuilder, BuilderPipelineBuilderContext>(model, new BuilderPipelineBuilderContext(sourceModel, settings, CultureInfo.InvariantCulture));
+
+            // Act
+            sut.Process(context);
+
+            // Assert
+            model.Interfaces.Should().BeEmpty();
+            model.GenericTypeArguments.Should().BeEmpty();
+            model.GenericTypeArgumentConstraints.Should().BeEmpty();
+            model.Abstract.Should().BeFalse();
+        }
+    }
+}
---
+++ src/ClassFramework.Pipelines.Tests/Extensions/ParentTypeContainerExtensionsTests.cs
@@ -0,0 +1,78 @@
+﻿namespace ClassFramework.Pipelines.Tests.Extensions;
+
+public class ParentTypeContainerExtensionsTests
+{
+    public class IsDefinedOn
+    {
+        [Fact]
+        public void Throws_On_Null_TypeBase_Argument()
+        {
+            // Arrange
+            var sut = new ClassPropertyBuilder().WithName("MyProperty").WithType(typeof(int)).Build();
+
+            // Act & Assert
+            sut.Invoking(x => x.IsDefinedOn(typeBase: null!))
+               .Should().Throw<ArgumentNullException>().WithParameterName("typeBase");
+        }
+
+        [Theory]
+        [InlineData(true, true)]
+        [InlineData(false, false)]
+        public void Returns_Result_From_ComparisonFunction_When_Provided(bool comparisonFunctionResult, bool expectedResult)
+        {
+            // Arrange
+            var sut = new ClassPropertyBuilder().WithName("MyProperty").WithType(typeof(int)).Build();
+            var typeBase = new ClassBuilder().WithName("MyClass").Build();
+
+            // Act
+            var result = sut.IsDefinedOn(typeBase, comparisonFunction: (_, _) => comparisonFunctionResult);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Returns_True_When_ParentTypeFullName_Is(string? parentTypeFullName)
+        {
+            // Arrange
+            var sut = new ClassPropertyBuilder().WithName("MyProperty").WithType(typeof(int)).WithParentTypeFullName(parentTypeFullName).Build();
+            var typeBase = new ClassBuilder().WithName("MyClass").Build();
+
+            // Act
+            var result = sut.IsDefinedOn(typeBase);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Returns_True_When_ParentTypeFullName_Is_Equal_To_TypeBase_FullName()
+        {
+            // Arrange
+            var typeBase = new ClassBuilder().WithName("MyClass").Build();
+            var sut = new ClassPropertyBuilder().WithName("MyProperty").WithType(typeof(int)).WithParentTypeFullName(typeBase.GetFullName()).Build();
+
+            // Act
+            var result = sut.IsDefinedOn(typeBase);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Returns_False_When_ParentTypeFullName_Is_Filled_But_Not_Equal_To_TypeBase_FullName()
+        {
+            // Arrange
+            var sut = new ClassPropertyBuilder().WithName("MyProperty").WithType(typeof(int)).WithParentTypeFullName("SomeOtherType").Build();
+            var typeBase = new ClassBuilder().WithName("MyClass").Build();
+
+            // Act
+            var result = sut.IsDefinedOn(typeBase);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+}
