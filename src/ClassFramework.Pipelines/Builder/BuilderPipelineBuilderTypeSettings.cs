9be9d10d2c487546dac1ff51468c7a23dd142bbc
Paul Deen <paul.deen@dutchworkz.nl>
2023-10-07 15:26:24 +0200

    Another YAGNI: Why use target-typed new expressions? Who cares, we are making generated code. Don't make this too complex...

--- src/ClassFramework.Pipelines/Builder/BuilderPipelineBuilderTypeSettings.cs
+++ src/ClassFramework.Pipelines/Builder/BuilderPipelineBuilderTypeSettings.cs
@@ -3,18 +3,15 @@
 public record BuilderPipelineBuilderTypeSettings
 {
     public string NewCollectionTypeName { get; }
-    public bool UseTargetTypeNewExpressions { get; }
     public bool EnableNullableReferenceTypes { get; }
     public Func<TypeBase, bool, string>? FormatInstanceTypeNameDelegate { get; }
 
     public BuilderPipelineBuilderTypeSettings(
         string newCollectionTypeName = "System.Collections.Generic.List",
-        bool useTargetTypeNewExpressions = true,
         bool enableNullableReferenceTypes = false,
         Func<TypeBase, bool, string>? formatInstanceTypeNameDelegate = null)
     {
         NewCollectionTypeName = newCollectionTypeName;
-        UseTargetTypeNewExpressions = useTargetTypeNewExpressions;
         EnableNullableReferenceTypes = enableNullableReferenceTypes;
         FormatInstanceTypeNameDelegate = formatInstanceTypeNameDelegate;
     }
