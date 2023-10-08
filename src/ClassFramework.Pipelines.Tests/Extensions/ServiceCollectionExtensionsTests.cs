0839f0204a8e7b9cd962573473ad7039852cf643
Paul Deen <paul.deen@dutchworkz.nl>
2023-10-08 14:38:48 +0200

    Added missing test for validating the Dependency Injection configuration

---
+++ src/ClassFramework.Pipelines.Tests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassFramework.Pipelines.Tests.Extensions;
+
+public class ServiceCollectionExtensionsTests : TestBase
+{
+    public class AddPipelines : ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void Registers_All_Required_Dependencies()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection()
+                .AddScoped(_ => Fixture.Freeze<IFormattableStringParser>()) // note that normally, you would probably use AddParsers from the CrossCutting.Utilities.Parsers package...
+                .AddPipelines();
+
+            // Act & Assert
+            serviceCollection.Invoking(x =>
+            {
+                using var provder = x.BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true, ValidateScopes = true });
+            }).Should().NotThrow();
+        }
+    }
+}
