a1d6e1cc3fb1aee4f22d3d2dc4bbf273cabcb74f
Paul Deen <paul.deen@dutchworkz.nl>
2023-10-13 08:44:58 +0200

    Added missing unit tests for ClassFramework.Domain project

---
+++ src/ClassFramework.Domain.Tests/Extensions/ObjectExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿namespace ClassFramework.Domain.Tests.Extensions;
+
+public class ObjectExtensionsTests
+{
+    [Fact]
+    public void CsharpFormat_Returns_Formatted_String_Value()
+    {
+        // Arrange
+        var input = "test";
+
+        // Act
+        var actual = input.CsharpFormat();
+
+        // Assert
+        actual.Should().Be(@"@""test""");
+    }
+
+    [Fact]
+    public void CsharpFormat_Returns_Boolean_String()
+    {
+        // Arrange
+        var input = false;
+
+        // Act
+        var actual = input.CsharpFormat();
+
+        // Assert
+        actual.Should().Be(@"false");
+    }
+
+    [Fact]
+    public void CsharpFormat_Returns_Int32_String()
+    {
+        // Arrange
+        var input = 12345;
+
+        // Act
+        var actual = input.CsharpFormat();
+
+        // Assert
+        actual.Should().Be(@"12345");
+    }
+}
---
+++ src/ClassFramework.Domain.Tests/LiteralTests.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassFramework.Domain.Tests;
+
+public class LiteralTests
+{
+    [Fact]
+    public void Constucts_Correclty_With_OriginalValue()
+    {
+        // Act
+        var sut = new Literal("value", "original value");
+
+        // Assert
+        sut.Value.Should().Be("value");
+        sut.OriginalValue.Should().Be("original value");
+    }
+
+    [Fact]
+    public void Constucts_Correclty_Without_OriginalValue()
+    {
+        // Act
+        var sut = new Literal("value");
+
+        // Assert
+        sut.Value.Should().Be("value");
+        sut.OriginalValue.Should().BeNull();
+    }
+}
