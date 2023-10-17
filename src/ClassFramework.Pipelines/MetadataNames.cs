8334b9fa5e63b74bc69813733d5dc4fb1c7573ab
Paul Deen <paul.deen@dutchworkz.nl>
2023-10-17 08:08:13 +0200

    Improved comments on metadata names, so they include the type it should be.

--- src/ClassFramework.Pipelines/MetadataNames.cs
+++ src/ClassFramework.Pipelines/MetadataNames.cs
@@ -3,7 +3,7 @@
 public static class MetadataNames
 {
     /// <summary>
-    /// Metadata name for defining a custom typename for argument in builder.
+    /// Metadata name for defining a custom typename for argument in builder. Note that the value will be converted to string.
     /// </summary>
     public const string CustomBuilderArgumentType = "ClassFramework.Builder.ArgumentType";
 
@@ -13,22 +13,22 @@ public static class MetadataNames
     public const string CustomBuilderDefaultValue = "ClassFramework.Builder.Ctor.CustomDefaultValue";
 
     /// <summary>
-    /// Metadata name for defining a custom initialization in the constructor of the builder.
+    /// Metadata name for defining a custom initialization in the constructor of the builder. Note that the value will be converted to string.
     /// </summary>
     public const string CustomBuilderConstructorInitializeExpression = "ClassFramework.Builder.ConstructorInitializeExpression";
 
     /// <summary>
-    /// Metadata name for defining a custom parameter expression in the builder.
+    /// Metadata name for defining a custom parameter expression in the builder. Note that the value will be converted to string.
     /// </summary>
     public const string CustomBuilderMethodParameterExpression = "ClassFramework.Builder.Ctor.MethodParameterExpression";
 
     /// <summary>
-    /// Metadata name for defining default value for a non-collection property on a fluent (With/Set) method in a builder.
+    /// Metadata name for defining default value for a property on a fluent (With/Set) method in a builder. Note that the value is of type System.Object, and will be rendered using CsharpFormat in code generation.
     /// </summary>
     public const string CustomBuilderWithDefaultPropertyValue = "ClassFramework.Builder.Property.DefaultValue";
 
     /// <summary>
-    /// Metadata name for defining a custom initialization on a fluent (With/Set) method
+    /// Metadata name for defining a custom initialization on a fluent (With/Set) method. Note that the value will be converted to string.
     /// </summary>
     public const string CustomBuilderWithExpression = "ClassFramework.Builder.WithExpression";
 }
