952a7cedda6f946e5405c5334bacaa3e32975101
Paul Deen <paul.deen@dutchworkz.nl>
2023-10-27 19:52:32 +0200

    Removed InheritFromInterfaces property from PipelineBuilderInheritanceSettings - we're not gonna use it

--- src/ClassFramework.Pipelines/Entity/PipelineBuilderInheritanceSettings.cs
+++ src/ClassFramework.Pipelines/Entity/PipelineBuilderInheritanceSettings.cs
@@ -4,20 +4,17 @@ public record PipelineBuilderInheritanceSettings
 {
     public bool EnableInheritance { get; }
     public bool IsAbstract { get; }
-    public bool InheritFromInterfaces { get; }
     public Class? BaseClass { get; }
     public Func<string, TypeBase, bool>? InheritanceComparisonDelegate { get; }
 
     public PipelineBuilderInheritanceSettings(
         bool enableInheritance = false,
         bool isAbstract = false,
-        bool inheritFromInterfaces = false,
         Class? baseClass = null,
         Func<string, TypeBase, bool>? inheritanceComparisonDelegate = null)
     {
         EnableInheritance = enableInheritance;
         IsAbstract = isAbstract;
-        InheritFromInterfaces = inheritFromInterfaces;
         BaseClass = baseClass;
         InheritanceComparisonDelegate = inheritanceComparisonDelegate;
     }
