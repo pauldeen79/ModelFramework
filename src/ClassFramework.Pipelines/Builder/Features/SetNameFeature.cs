1e94f12112b04f55748d3dd4e2807e186dd4a354
Paul Deen <paul.deen@dutchworkz.nl>
2023-10-31 15:27:20 +0100

    Replaced two more GetValueOrThrow calls

--- src/ClassFramework.Pipelines/Builder/Features/SetNameFeature.cs
+++ src/ClassFramework.Pipelines/Builder/Features/SetNameFeature.cs
@@ -26,8 +26,21 @@ public class SetNameFeature : IPipelineFeature<ClassBuilder, BuilderContext>
     {
         context = context.IsNotNull(nameof(context));
 
-        context.Model.Name = _formattableStringParser.Parse(context.Context.Settings.NameSettings.BuilderNameFormatString, context.Context.FormatProvider, context).GetValueOrThrow();
-        context.Model.Namespace = _formattableStringParser.Parse(context.Context.Settings.NameSettings.BuilderNamespaceFormatString, context.Context.FormatProvider, context).GetValueOrThrow();
+        var results = new[]
+{
+            new { Name = "Name", Result = _formattableStringParser.Parse(context.Context.Settings.NameSettings.BuilderNameFormatString, context.Context.FormatProvider, context) },
+            new { Name = "Namespace", Result = _formattableStringParser.Parse(context.Context.Settings.NameSettings.BuilderNamespaceFormatString, context.Context.FormatProvider, context) },
+        }.TakeWhileWithFirstNonMatching(x => x.Result.IsSuccessful()).ToArray();
+
+        var error = Array.Find(results, x => !x.Result.IsSuccessful());
+        if (error is not null)
+        {
+            // Error in formattable string parsing
+            return Result.FromExistingResult<ClassBuilder>(error.Result);
+        }
+
+        context.Model.Name = results.First(x => x.Name == "Name").Result.Value!;
+        context.Model.Namespace = results.First(x => x.Name == "Namespace").Result.Value!;
 
         return Result.Continue<ClassBuilder>();
     }
