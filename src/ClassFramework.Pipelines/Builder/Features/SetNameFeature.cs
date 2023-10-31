0d26042d25464cd3a72ecc4314c881086a64f1fb
Paul Deen <paul.deen@dutchworkz.nl>
2023-10-31 15:39:24 +0100

    Made string parsing evaluation lazy, so it stops on first non successful result

--- src/ClassFramework.Pipelines/Builder/Features/SetNameFeature.cs
+++ src/ClassFramework.Pipelines/Builder/Features/SetNameFeature.cs
@@ -28,19 +28,19 @@ public class SetNameFeature : IPipelineFeature<ClassBuilder, BuilderContext>
 
         var results = new[]
 {
-            new { Name = "Name", Result = _formattableStringParser.Parse(context.Context.Settings.NameSettings.BuilderNameFormatString, context.Context.FormatProvider, context) },
-            new { Name = "Namespace", Result = _formattableStringParser.Parse(context.Context.Settings.NameSettings.BuilderNamespaceFormatString, context.Context.FormatProvider, context) },
-        }.TakeWhileWithFirstNonMatching(x => x.Result.IsSuccessful()).ToArray();
+            new { Name = "Name", LazyResult = new Lazy<Result<string>>(() => _formattableStringParser.Parse(context.Context.Settings.NameSettings.BuilderNameFormatString, context.Context.FormatProvider, context)) },
+            new { Name = "Namespace", LazyResult = new Lazy<Result<string>>(() => _formattableStringParser.Parse(context.Context.Settings.NameSettings.BuilderNamespaceFormatString, context.Context.FormatProvider, context)) },
+        }.TakeWhileWithFirstNonMatching(x => x.LazyResult.Value.IsSuccessful()).ToArray();
 
-        var error = Array.Find(results, x => !x.Result.IsSuccessful());
+        var error = Array.Find(results, x => !x.LazyResult.Value.IsSuccessful());
         if (error is not null)
         {
             // Error in formattable string parsing
-            return Result.FromExistingResult<ClassBuilder>(error.Result);
+            return Result.FromExistingResult<ClassBuilder>(error.LazyResult.Value);
         }
 
-        context.Model.Name = results.First(x => x.Name == "Name").Result.Value!;
-        context.Model.Namespace = results.First(x => x.Name == "Namespace").Result.Value!;
+        context.Model.Name = results.First(x => x.Name == "Name").LazyResult.Value.Value!;
+        context.Model.Namespace = results.First(x => x.Name == "Namespace").LazyResult.Value.Value!;
 
         return Result.Continue<ClassBuilder>();
     }
