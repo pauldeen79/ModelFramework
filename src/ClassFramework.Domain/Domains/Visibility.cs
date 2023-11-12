b7b57312e7ad04e4b3a43013f8761cdea2c84494
Paul Deen <paul.deen@dutchworkz.nl>
2023-11-12 15:37:56 +0100

    Added some xml documentation to enums

--- src/ClassFramework.Domain/Domains/Visibility.cs
+++ src/ClassFramework.Domain/Domains/Visibility.cs
@@ -2,7 +2,16 @@
 
 public enum Visibility
 {
+    /// <summary>
+    /// Visible to everyone
+    /// </summary>
     Public,
+    /// <summary>
+    /// Visible to the class itself, and all other classes within the same assembly
+    /// </summary>
     Internal,
+    /// <summary>
+    /// Visible to the class itself only
+    /// </summary>
     Private
 }
--- src/ClassFramework.Pipelines/Domains/ArgumentValidationType.cs
+++ src/ClassFramework.Pipelines/Domains/ArgumentValidationType.cs
@@ -2,7 +2,16 @@
 
 public enum ArgumentValidationType
 {
+    /// <summary>
+    /// Do not validate arguments
+    /// </summary>
     None,
+    /// <summary>
+    /// Validate arguments in builder and domain entity, re-using the domain entity validation code
+    /// </summary>
     Shared,
+    /// <summary>
+    /// Validate arguments in domain entity only. When building the entity from the builder, the entity will validate itself.
+    /// </summary>
     DomainOnly
 }
