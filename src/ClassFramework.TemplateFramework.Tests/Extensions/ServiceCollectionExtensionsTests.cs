4fa8e467392e9fa55764a9362a1b47e3cfd14f13
Paul Deen <paul.deen@dutchworkz.nl>
2023-11-27 12:00:56 +0100

    Added missing unit tests

---
+++ src/ClassFramework.TemplateFramework.Tests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassFramework.TemplateFramework.Tests.Extensions;
+
+public class ServiceCollectionExtensionsTests
+{
+    public class AddClassFrameworkTemplates : ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void Registers_All_Required_Dependencies()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection()
+                .AddCsharpExpressionCreator() // dependency of some templates
+                .AddClassFrameworkTemplates();
+
+            // Act & Assert
+            serviceCollection.Invoking(x =>
+            {
+                using var provder = x.BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true, ValidateScopes = true });
+            }).Should().NotThrow();
+        }
+    }
+}
