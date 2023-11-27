fc5bc6e01b876f2b91f2736b84a58b11fd26fff3
Paul Deen <paul.deen@dutchworkz.nl>
2023-11-27 16:05:31 +0100

    Refactored to switch expression, checking against IBaseClassContainer instead of Class, and simplified code to get inherited classes (no more two separate methods)

--- src/ClassFramework.TemplateFramework/ViewModels/TypeBaseViewModel.cs
+++ src/ClassFramework.TemplateFramework/ViewModels/TypeBaseViewModel.cs
@@ -12,52 +12,33 @@ public class TypeBaseViewModel : CsharpClassGeneratorViewModel<TypeBase>
     public bool ShouldRenderNamespaceScope
         => Settings.GenerateMultipleFiles && !string.IsNullOrEmpty(Data.Namespace);
 
-    public string GetInheritedClasses()
-        => Data is Class cls
-            ? GetInheritedClassesForClass(cls)
-            : GetInheritedClassesForTypeBase();
-
     public string GetContainerType()
-    {
-        if (Data is Class cls)
+        => Data switch
         {
-            return cls.Record
+            Class cls => cls.Record
                 ? "record"
-                : "class";
-        }
-
-        if (Data is Struct str)
-        {
-            return str.Record
+                : "class",
+            Struct str => str.Record
                 ? "record struct"
-                : "struct";
-        }
+                : "struct",
+            Interface => "interface",
+            _ => throw new InvalidOperationException($"Unknown container type: [{Data.GetType().FullName}]")
+        };
 
-        if (Data is Interface)
-        {
-            return "interface";
-        }
-
-        throw new InvalidOperationException($"Unknown container type: [{Data.GetType().FullName}]");
-    }
-
-    private static string GetInheritedClassesForClass(Class cls)
+    public string GetInheritedClasses()
     {
         var lst = new List<string>();
-        if (!string.IsNullOrEmpty(cls.BaseClass))
+
+        var baseClassContainer = Data as IBaseClassContainer;
+        if (!string.IsNullOrEmpty(baseClassContainer?.BaseClass))
         {
-            lst.Add(cls.BaseClass);
+            lst.Add(baseClassContainer.BaseClass);
         }
 
-        lst.AddRange(cls.Interfaces);
+        lst.AddRange(Data.Interfaces);
 
         return lst.Count == 0
             ? string.Empty
             : $" : {string.Join(", ", lst.Select(x => x.GetCsharpFriendlyTypeName()))}";
     }
-
-    private string GetInheritedClassesForTypeBase()
-        => Data.Interfaces.Count == 0
-            ? string.Empty
-            : $" : {string.Join(", ", Data.Interfaces.Select(x => x.GetCsharpFriendlyTypeName()))}";
 }
