79e1e96dfea3641bfd8a3f3c5849f942ae731357
Paul Deen <paul.deen@dutchworkz.nl>
2023-12-03 17:50:38 +0100

    Refactor: Moved IsInterfaceMethod extension method to Domain, and making it a bit more full-proof for interface name detection - the second character should be upper case as well

---
+++ src/ClassFramework.Domain/ClassMethod.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassFramework.Domain;
+
+public partial record ClassMethod
+{
+    public bool IsInterfaceMethod()
+        => Name.StartsWith("I", StringComparison.Ordinal)
+        && Name.Length > 1
+        && Name.Equals(Name.Substring(1, 1), StringComparison.OrdinalIgnoreCase)
+        && Name.Contains('.');
+}
