e2442f6a41e927234d654489979e3b7c0ea0f04b
Paul Deen <paul.deen@dutchworkz.nl>
2023-12-07 14:22:46 +0100

    Commented unused code

--- src/ClassFramework.TemplateFramework/ViewModelTemplateParameterConverter.cs
+++ src/ClassFramework.TemplateFramework/ViewModelTemplateParameterConverter.cs
@@ -1,55 +1,55 @@
-﻿namespace ClassFramework.TemplateFramework;
+﻿//namespace ClassFramework.TemplateFramework;
 
-public class ViewModelTemplateParameterConverter : ITemplateParameterConverter
-{
-    private readonly IEnumerable<IViewModel> _viewModels;
+//public class ViewModelTemplateParameterConverter : ITemplateParameterConverter
+//{
+//    private readonly IEnumerable<IViewModel> _viewModels;
 
-    public ViewModelTemplateParameterConverter(IEnumerable<IViewModel> viewModels)
-    {
-        Guard.IsNotNull(viewModels);
+//    public ViewModelTemplateParameterConverter(IEnumerable<IViewModel> viewModels)
+//    {
+//        Guard.IsNotNull(viewModels);
 
-        _viewModels = viewModels;
-    }
+//        _viewModels = viewModels;
+//    }
 
-    public bool TryConvert(object? value, Type type, ITemplateEngineContext context, out object? convertedValue)
-    {
-        if (value is null)
-        {
-            convertedValue = null;
-            return false;
-        }
+//    public bool TryConvert(object? value, Type type, ITemplateEngineContext context, out object? convertedValue)
+//    {
+//        if (value is null)
+//        {
+//            convertedValue = null;
+//            return false;
+//        }
 
-        var viewModel = _viewModels.FirstOrDefault(x => Supports(x, value));
-        if (viewModel is null)
-        {
-            convertedValue = null;
-            return false;
-        }
+//        var viewModel = _viewModels.FirstOrDefault(x => Supports(x, value));
+//        if (viewModel is null)
+//        {
+//            convertedValue = null;
+//            return false;
+//        }
 
-        convertedValue = viewModel;
+//        convertedValue = viewModel;
 
-        // Copy Model to ViewModel
-        var prop = viewModel.GetType().GetProperty(nameof(IModelContainer<object>.Model));
-        if (prop is not null && prop.GetValue(viewModel) is null && prop.PropertyType.IsInstanceOfType(value))
-        {
-            prop.SetValue(viewModel, value);
-        }
+//        // Copy Model to ViewModel
+//        var prop = viewModel.GetType().GetProperty(nameof(IModelContainer<object>.Model));
+//        if (prop is not null && prop.GetValue(viewModel) is null && prop.PropertyType.IsInstanceOfType(value))
+//        {
+//            prop.SetValue(viewModel, value);
+//        }
         
-        // Copy Settings to ViewUodel
-        if (viewModel is ICsharpClassGeneratorSettingsContainer container && container.Settings is null && context?.AdditionalParameters is ICsharpClassGeneratorSettingsContainer container2)
-        {
-            container.Settings = container2.Settings;
-        }
+//        // Copy Settings to ViewUodel
+//        if (viewModel is ICsharpClassGeneratorSettingsContainer container && container.Settings is null && context?.AdditionalParameters is ICsharpClassGeneratorSettingsContainer container2)
+//        {
+//            container.Settings = container2.Settings;
+//        }
 
-        return true;
-    }
+//        return true;
+//    }
 
-    private bool Supports(object viewModel, object? model)
-    {
-        var viewModelType = viewModel.GetType();
-        var prop = viewModelType.GetProperty(nameof(IModelContainer<object>.Model));
+//    private bool Supports(object viewModel, object? model)
+//    {
+//        var viewModelType = viewModel.GetType();
+//        var prop = viewModelType.GetProperty(nameof(IModelContainer<object>.Model));
 
-        return prop is not null
-            && prop.PropertyType.IsInstanceOfType(model);
-    }
-}
+//        return prop is not null
+//            && prop.PropertyType.IsInstanceOfType(model);
+//    }
+//}
