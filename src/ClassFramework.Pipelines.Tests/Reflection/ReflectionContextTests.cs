6a968b73030e1814b01ecb7d10778873e7f265fc
Paul Deen <paul.deen@dutchworkz.nl>
2023-12-15 15:33:58 +0100

    Added two missing unit tests

--- src/ClassFramework.Pipelines.Tests/Reflection/ReflectionContextTests.cs
+++ src/ClassFramework.Pipelines.Tests/Reflection/ReflectionContextTests.cs
@@ -28,4 +28,36 @@ public class ReflectionContextTests : TestBase
                 .Should().Throw<ArgumentNullException>().WithParameterName("formatProvider");
         }
     }
+
+    public class MapTypeName : ReflectionContextTests
+    {
+        [Fact]
+        public void Throws_On_Null_TypeName()
+        {
+            // Arrange
+            var settings = CreateBuilderSettings(enableNullableReferenceTypes: false);
+            var sut = new BuilderContext(CreateModel(), settings, CultureInfo.InvariantCulture);
+
+            // Act & Assert
+            sut.Invoking(x => x.MapTypeName(typeName: null!))
+               .Should().Throw<ArgumentNullException>()
+               .WithParameterName("typeName");
+        }
+    }
+
+    public class MapAttribute : ReflectionContextTests
+    {
+        [Fact]
+        public void Throws_On_Null_TypeName()
+        {
+            // Arrange
+            var settings = CreateReflectionSettings();
+            var sut = new ReflectionContext(GetType(), settings, CultureInfo.InvariantCulture);
+
+            // Act & Assert
+            sut.Invoking(x => x.MapAttribute(attribute: null!))
+               .Should().Throw<ArgumentNullException>()
+               .WithParameterName("attribute");
+        }
+    }
 }
