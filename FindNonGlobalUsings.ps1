077a18630aebb60c54109e1d3f3bf851799073c3
Paul Deen <paul.deen@dutchworkz.nl>
2023-12-17 19:57:50 +0100

    Improved FindNonGlobalUsings script. We should stop

--- FindNonGlobalUsings.ps1
+++ FindNonGlobalUsings.ps1
@@ -2,9 +2,6 @@
 $directoryPath = Get-Location
 $fileExtension = "*.cs"
 
-# Set the search phrase
-$searchPhrase = "using *"  # Change this to your desired search phrase
-
 # Get all files with the specified extension in the directory
 $fileList = Get-ChildItem -Path $directoryPath -Filter $fileExtension -Recurse | Where-Object { $_.FullName -notlike '*\obj\*' -and $_.FullName -notlike '*.generated.cs' }
 
@@ -13,7 +10,13 @@ foreach ($file in $fileList) {
     $fileContent = Get-Content $file.FullName
 
     foreach ($line in $fileContent) {
-        if ($line -like "$searchPhrase*") {
+        if ($line -like "namespace *") {
+            # stop searching for using.
+            # if it's found after the namespace, then it's probably in a string literal
+            break
+        }
+
+        if ($line -like "using *") {
             Write-Host "$($file.FullName):$line"
             break
         }
