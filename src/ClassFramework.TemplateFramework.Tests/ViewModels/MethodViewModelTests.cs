44f1f63e6dd1f69d927848389a38314427c65fb8
Paul Deen <paul.deen@dutchworkz.nl>
2023-12-21 17:17:56 +0100

    Added some tests for MethodViewModel class

--- src/ClassFramework.TemplateFramework.Tests/ViewModels/MethodViewModelTests.cs
+++ src/ClassFramework.TemplateFramework.Tests/ViewModels/MethodViewModelTests.cs
@@ -39,8 +39,8 @@ public class MethodViewModelTests : TestBase<MethodViewModel>
         [InlineData(null, "interface", false)]
         [InlineData("", "class", true)]
         [InlineData("", "interface", false)]
-        [InlineData("prefix", "class", false)]
-        [InlineData("prefix", "interface", false)]
+        [InlineData("IMyInterface", "class", false)]
+        [InlineData("IMyInterface", "interface", false)]
         public void Returns_Correct_Result(string? explicitInterfaceName, string parentModelType, bool expectedResult)
         {
             // Arrange
@@ -85,7 +85,10 @@ public class MethodViewModelTests : TestBase<MethodViewModel>
         {
             // Arrange
             var sut = CreateSut();
-            sut.Model = new MethodBuilder().WithName("MyMethod").WithReturnType(typeof(int)).Build();
+            sut.Model = new MethodBuilder()
+                .WithName("MyMethod")
+                .WithReturnType(typeof(int))
+                .Build();
 
             // Act
             var result = sut.ReturnTypeName;
@@ -99,13 +102,115 @@ public class MethodViewModelTests : TestBase<MethodViewModel>
         {
             // Arrange
             var sut = CreateSut();
-            sut.Model = new MethodBuilder().WithName("MyMethod").WithReturnType(new ClassBuilder().WithName("MyType")).WithReturnTypeIsNullable().Build();
+            sut.Model = new MethodBuilder()
+                .WithName("MyMethod")
+                .WithReturnType(new ClassBuilder().WithName("MyType"))
+                .WithReturnTypeIsNullable()
+                .Build();
 
             // Act
             var result = sut.ReturnTypeName;
 
             // Assert
             result.Should().Be("MyType?");
+        }
+
+        [Fact]
+        public void Abbreviates_Namespaces()
+        {
+            // Arrange
+            var sut = CreateSut();
+            sut.Model = new MethodBuilder()
+                .WithName("MyMethod")
+                .WithReturnType(new ClassBuilder().WithName("MyType").WithNamespace("MyNamespace"))
+                .AddMetadata(MetadataNames.NamespaceToAbbreviate, "MyNamespace")
+                .Build();
+
+            // Act
+            var result = sut.ReturnTypeName;
+
+            // Assert
+            result.Should().Be("MyType");
+        }
+
+        [Fact]
+        public void Returns_void_When_Empty()
+        {
+            // Arrange
+            var sut = CreateSut();
+            sut.Model = new MethodBuilder()
+                .WithName("MyMethod")
+                .WithReturnTypeName(string.Empty)
+                .Build();
+
+            // Act
+            var result = sut.ReturnTypeName;
+
+            // Assert
+            result.Should().Be("void");
+        }
+    }
+
+    public class ExplicitInterfaceName : MethodViewModelTests
+    {
+        [Fact]
+        public void Throws_When_Model_Is_Null()
+        {
+            // Arrange
+            var sut = CreateSut();
+            sut.Model = null!;
+            var context = Fixture.Freeze<ITemplateContext>();
+            context.ParentContext.Returns(context); // note that we're using short-circuit here 8-) but who cares, we're just calling ParentContext.Model so it works.
+            sut.Context = context;
+
+            // Act & Assert
+            sut.Invoking(x => _ = x.ExplicitInterfaceName)
+               .Should().Throw<ArgumentNullException>()
+               .WithParameterName("Model");
+        }
+
+        [Fact]
+        public void Throws_When_Context_Is_Null()
+        {
+            // Arrange
+            var sut = CreateSut();
+            sut.Model = new MethodBuilder().WithName("MyMethod").WithExplicitInterfaceName("IMyInterface").Build();
+            sut.Context = null!;
+
+            // Act & Assert
+            sut.Invoking(x => _ = x.ExplicitInterfaceName)
+               .Should().Throw<ArgumentNullException>()
+               .WithParameterName("Context");
+        }
+
+        [Theory]
+        [InlineData(null, "class", "")]
+        [InlineData(null, "interface", "")]
+        [InlineData("", "class", "")]
+        [InlineData("", "interface", "")]
+        [InlineData("IMyInterface", "class", "IMyInterface.")]
+        [InlineData("IMyInterface", "interface", "")]
+        public void Returns_Correct_Result(string? explicitInterfaceName, string parentModelType, string expectedResult)
+        {
+            // Arrange
+            object? parentModel = parentModelType switch
+            {
+                "class" => new ClassBuilder().WithName("MyClass").Build(),
+                "interface" => new InterfaceBuilder().WithName("IMyInterface").Build(),
+                _ => throw new NotSupportedException("Only 'class' and 'interface' are supported as parentModelType")
+            };
+            var sut = CreateSut();
+            sut.Model = new MethodBuilder().WithName("MyMethod").WithExplicitInterfaceName(explicitInterfaceName).Build();
+            var context = Fixture.Freeze<ITemplateContext>();
+            context.ParentContext.Returns(context); // note that we're using short-circuit here 8-) but who cares, we're just calling ParentContext.Model so it works.
+            context.Model.Returns(parentModel);
+            sut.Context = context;
+
+            // Act
+            var result = sut.ExplicitInterfaceName;
+
+            // Assert
+            result.Should().Be(expectedResult);
         }
     }
 }
