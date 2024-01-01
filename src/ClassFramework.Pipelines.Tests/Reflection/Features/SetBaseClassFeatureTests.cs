5a2598eaabbbaf1a58ff7fa6a296870250dd089a
Paul Deen <paul.deen@dutchworkz.nl>
2024-01-01 09:32:37 +0100

    Fixed code analysis warning in test code

--- src/ClassFramework.Pipelines.Tests/Reflection/Features/SetBaseClassFeatureTests.cs
+++ src/ClassFramework.Pipelines.Tests/Reflection/Features/SetBaseClassFeatureTests.cs
@@ -119,8 +119,10 @@ internal sealed class MyBaseClassTestClass : MyBaseClassTestClassBase
 {
 }
 
+#pragma warning disable CA1812 // Avoid uninstantiated internal classes
 #pragma warning disable S2094 // Classes should not be empty
 internal class MyBaseClassTestClassBase
+#pragma warning restore CA1812 // Avoid uninstantiated internal classes
 #pragma warning restore S2094 // Classes should not be empty
 {
 }
