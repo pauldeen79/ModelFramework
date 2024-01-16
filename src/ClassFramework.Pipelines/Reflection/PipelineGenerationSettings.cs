f36e37d745fc7902682590bf78b4466d952048c1
Paul Deen <paul.deen@dutchworkz.nl>
2024-01-16 12:00:38 +0100

    Improved support for a number of validation attributes in reflection pipeline (adding properties)

--- src/ClassFramework.Pipelines/Reflection/PipelineGenerationSettings.cs
+++ src/ClassFramework.Pipelines/Reflection/PipelineGenerationSettings.cs
@@ -23,5 +23,49 @@ public record PipelineGenerationSettings
     }
 
     private static AttributeBuilder DefaultInitializer(System.Attribute sourceAttribute)
-        =>  new AttributeBuilder().WithName(sourceAttribute.GetType());
+        => sourceAttribute switch
+        {
+            StringLengthAttribute stringLengthAttribute =>
+                new AttributeBuilder().WithName(stringLengthAttribute.GetType())
+                    .AddParameters(new AttributeParameterBuilder().WithValue(stringLengthAttribute.MaximumLength))
+                    .AddParameters(CreateConditional(() => stringLengthAttribute.MinimumLength > 0, () => new AttributeParameterBuilder().WithValue(stringLengthAttribute.MinimumLength).WithName(nameof(stringLengthAttribute.MinimumLength))))
+                    .AddParameters(ErrorMessage(stringLengthAttribute)),
+            RangeAttribute rangeAttribute =>
+                new AttributeBuilder().WithName(rangeAttribute.GetType())
+                    .AddParameters(new AttributeParameterBuilder().WithValue(rangeAttribute.Minimum))
+                    .AddParameters(new AttributeParameterBuilder().WithValue(rangeAttribute.Maximum))
+                    .AddParameters(ErrorMessage(rangeAttribute)),
+            MinLengthAttribute minLengthAttribute =>
+                new AttributeBuilder().WithName(minLengthAttribute.GetType())
+                    .AddParameters(new AttributeParameterBuilder().WithValue(minLengthAttribute.Length))
+                    .AddParameters(ErrorMessage(minLengthAttribute)),
+            MaxLengthAttribute maxLengthAttribute =>
+                new AttributeBuilder().WithName(maxLengthAttribute.GetType())
+                    .AddParameters(new AttributeParameterBuilder().WithValue(maxLengthAttribute.Length))
+                    .AddParameters(ErrorMessage(maxLengthAttribute)),
+            RegularExpressionAttribute regularExpressionAttribute =>
+                new AttributeBuilder().WithName(regularExpressionAttribute.GetType())
+                    .AddParameters(new AttributeParameterBuilder().WithValue(regularExpressionAttribute.Pattern))
+                    .AddParameters(CreateConditional(() => regularExpressionAttribute.MatchTimeoutInMilliseconds != 2000, () => new AttributeParameterBuilder().WithValue(regularExpressionAttribute.MatchTimeoutInMilliseconds).WithName(nameof(RegularExpressionAttribute.MatchTimeoutInMilliseconds))))
+                    .AddParameters(ErrorMessage(regularExpressionAttribute)),
+            RequiredAttribute requiredAttribute =>
+                new AttributeBuilder().WithName(requiredAttribute.GetType())
+                    .AddParameters(CreateConditional(() => requiredAttribute.AllowEmptyStrings, () => new AttributeParameterBuilder().WithValue(requiredAttribute.AllowEmptyStrings).WithName(nameof(RequiredAttribute.AllowEmptyStrings))))
+                    .AddParameters(ErrorMessage(requiredAttribute)),
+            ValidationAttribute validationAttribute =>
+                new AttributeBuilder().WithName(validationAttribute.GetType())
+                    .AddParameters(ErrorMessage(validationAttribute)),
+            _ => new AttributeBuilder().WithName(sourceAttribute.GetType())
+        };
+
+    private static IEnumerable<AttributeParameterBuilder> ErrorMessage(ValidationAttribute validationAttribute)
+        => CreateConditional(() => !string.IsNullOrEmpty(validationAttribute.ErrorMessage), () => new AttributeParameterBuilder().WithValue(validationAttribute.ErrorMessage).WithName(nameof(ValidationAttribute.ErrorMessage)));
+
+    private static IEnumerable<AttributeParameterBuilder> CreateConditional(Func<bool> condition, Func<AttributeParameterBuilder> result)
+    {
+        if (condition.Invoke())
+        {
+            yield return result.Invoke();
+        }
+    }
 }
