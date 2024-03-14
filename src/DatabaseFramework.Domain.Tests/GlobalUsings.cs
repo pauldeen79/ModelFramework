8363c5b1300816a0252f2eb4ca6705e60fd5cc92
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-14 10:53:56 +0100

    Added test project for DatabaseFramework.Domain

---
+++ src/DatabaseFramework.Domain.Tests/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using DatabaseFramework.Domain.Builders;
+global using FluentAssertions;
---
+++ src/DatabaseFramework.Domain.Tests/TableTests.cs
@@ -0,0 +1,14 @@
+﻿namespace DatabaseFramework.Domain.Tests;
+
+public class TableTests
+{
+    [Fact]
+    public void Cannot_Construct_Without_Any_Fields()
+    {
+        // Arrange
+        var tableBuilder = new TableBuilder().WithName("MyTable");
+
+        // Act & Assert
+        tableBuilder.Invoking(x => x.Build()).Should().Throw<ValidationException>().WithMessage("The field Fields is invalid.");
+    }
+}
