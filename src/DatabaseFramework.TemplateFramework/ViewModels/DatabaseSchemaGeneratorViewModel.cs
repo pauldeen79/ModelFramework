f572e049dc3e5c11873885d973b4c9d2721275c4
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-18 13:53:30 +0100

    Added ordering of database objects based on type, backwards compatible with ModelFramework.Database. Also changed rendering, so foreign keys are included.

--- src/DatabaseFramework.TemplateFramework/ViewModels/DatabaseSchemaGeneratorViewModel.cs
+++ src/DatabaseFramework.TemplateFramework/ViewModels/DatabaseSchemaGeneratorViewModel.cs
@@ -5,6 +5,29 @@ public class DatabaseSchemaGeneratorViewModel : DatabaseSchemaGeneratorViewModel
     public IOrderedEnumerable<IGrouping<string, IDatabaseObject>> Schemas
         => GetModel().GroupBy(x => x.Schema).OrderBy(x => x.Key);
 
-    public IEnumerable<IDatabaseObject> GetDatabaseObjects(IEnumerable<IDatabaseObject> schema)
-        => schema.OrderBy(item => item.Name);
+    public IEnumerable<INameContainer> GetDatabaseObjects(IEnumerable<INameContainer> objects)
+        => objects.SelectMany(GetItemsWithForeignKeyConstraints).OrderBy(GetOrder).ThenBy(item => item.Name);
+
+    private IEnumerable<INameContainer> GetItemsWithForeignKeyConstraints(INameContainer item)
+    {
+        yield return item;
+
+        if (item is Table table)
+        {
+            foreach (var fk in table.ForeignKeyConstraints)
+            {
+                yield return fk;
+            }
+        }
+    }
+
+    private static int GetOrder(INameContainer item)
+        => item switch
+        {
+            Table => 1,
+            ForeignKeyConstraint => 2,
+            StoredProcedure => 3,
+            View => 4,
+            _ => 99
+        }; // note that this order is backwards compatible with ModelFramework.Database :-)
 }
